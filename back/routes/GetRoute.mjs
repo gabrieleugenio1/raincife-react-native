14b30a20fdee909964202f58fb75daca6aac4e3a
gabrieleugenio1 <101233631+gabrieleugenio1@users.noreply.github.com>
2023-12-04 21:32:29 -0300

    exclusão rotas

--- back/routes/GetRoute.mjs
+++ back/routes/GetRoute.mjs
@@ -8,12 +8,5 @@ import Middleware from '../middlewares/Middlewares.mjs';
 const router = Router();
 
 router
-      .get('/', GetController.index)
-      .get('/cadastro', Middleware.jaEstaAutenticado, GetController.cadastro)
-      .get('/login', Middleware.jaEstaAutenticado, GetController.login)
-      .get('/esqueci-senha', Middleware.jaEstaAutenticado, GetController.esqueceuSenha)
-      .get('/home', Middleware.autorizacao, GetController.home)
       .get('/logout', Middleware.autorizacao, GetController.logout)
-      .get('/admin', Middleware.autorizacao, Middleware.admin, GetController.admin)
-      .get('/admin/user/:id', Middleware.admin, GetController.alterarUser)
 export default router;
